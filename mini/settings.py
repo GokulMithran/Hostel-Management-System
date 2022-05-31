ef6b95377b85f496238c49084f1fa3b01b54f62e
GokulMithran <gokulnth781@gmail.com>
2022-05-31 21:23:10 +0530

    First Push

--- mini/settings.py
+++ mini/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-c)msmaanm=jz=28=0ess9xmko8fu0v1_d1jnt_)nnorik=qp-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['miniproject-hms.herokuapp.com']
 
 
 # Application definition
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mini.urls'
@@ -80,12 +84,12 @@ WSGI_APPLICATION = 'mini.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mini',
-        'USER':'postgres',
-        'PASSWORD':'a',
-        'HOST':'localhost',
-        'PORT':'5433',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd920tsrifddqag',
+        'USER':'vrvdinswmvgajd',
+        'PASSWORD':'6708fa2c67c65f55a097c763c013478b9e16837b5dbd7bf3dcf088d572ec4a3a',
+        'HOST':'ec2-34-231-221-151.compute-1.amazonaws.com',
+        'PORT':'5432',
     }
 }
 
@@ -130,11 +134,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # STATIC_ROOT =os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
