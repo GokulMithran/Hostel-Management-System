3b763def09b9ca487c3e029b334f5965895f7504
GokulMithran <gokulnth781@gmail.com>
2022-05-31 23:07:27 +0530

    First Push

--- mini/settings.py
+++ mini/settings.py
@@ -140,7 +140,10 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = "/static/"
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # STATIC_ROOT =os.path.join(BASE_DIR,'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 django_heroku.settings(locals())
