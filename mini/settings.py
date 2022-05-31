ae27f47b401310efb0fd3f2d0271cad14a4255ba
GokulMithran <gokulnth781@gmail.com>
2022-05-31 22:54:46 +0530

    First Push

--- mini/settings.py
+++ mini/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'mini.urls'
@@ -139,7 +140,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = "/static/"
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # STATIC_ROOT =os.path.join(BASE_DIR,'media')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 django_heroku.settings(locals())
