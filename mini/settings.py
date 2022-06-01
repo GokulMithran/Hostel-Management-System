b95f8a1ef096be8b9767192714093e5da7b300c3
GokulMithran <104688792+GokulMithran@users.noreply.github.com>
2022-06-01 21:38:04 +0530

    Update settings.py

--- mini/settings.py
+++ mini/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 #STATICFILES_DIRS=[
 #    os.path.join(BASE_DIR,'static')
 #]
-STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = "/static/"
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
